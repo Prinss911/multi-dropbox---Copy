458c93bbb41304a2577a15001944bdbe0884da4c
ilyasanf <ilyasa.naufal40@gmail.com>
2026-01-02 22:58:04 +0700

    feat: Add default redirect from root to / to /upload page.

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,4 @@
-<template>
-  <FileBrowser />
-</template>
-
 <script setup lang="ts">
-// Langsung tampilkan FileBrowser
-// Tidak perlu login karena credentials sudah di .env
+// Redirect to /upload by default
+await navigateTo('/upload')
 </script>
